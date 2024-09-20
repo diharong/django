e780ee1e6d82faaf6156fd477e30240b34467583
diharong <diharong01@gmail.com>
2024-09-20 17:07:33 +0900

    up

---
+++ 03-model/articles/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+# class Article(상위클래스):
+
+class Article(models.Model):
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
